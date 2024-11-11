570174a367f577356f74979741e3066a4eb79df6
Hunrax <radoslawgajewski03@gmail.com>
2024-11-11 19:40:30 +0100

    added sending messages to rabbit

--- generator/main.py
+++ generator/main.py
@@ -1,5 +1,6 @@
 import random
 import time
+import pika
 from datetime import datetime
 from sensor_ids import *
 
@@ -26,80 +27,94 @@ INTAKE_AIR_TEMP_TOP_LIMIT = 40
 
 TEMPERATURE_ROUNDING = 1
 
+RABBITMQ_HOST = "localhost" 
+QUEUE_NAME = "messages"
+
 def createMessage(sensorId, value):
-    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    current_time = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
     return f"{{\"sensor_id\": \"{sensorId}\", \"value\": {value}, \"timestamp\": \"{current_time}\"}}\n"
 
 
-with open("generator/GeneratedData.txt", "w", encoding="utf8") as datafile:
+connection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQ_HOST))
+channel = connection.channel()
+
+channel.queue_declare(queue=QUEUE_NAME)
+
+try:
     while True:
         # temperature data
         engine_temperature = round(random.uniform(ENGINE_TEMP_BOTTOM_LIMIT, ENGINE_TEMP_TOP_LIMIT), TEMPERATURE_ROUNDING)
         message = createMessage(ENGINE_TEMP_ID, engine_temperature)
-        datafile.write(message)
+        channel.basic_publish(exchange='', routing_key=QUEUE_NAME, body=message)
 
         cabin_temperature = round(random.uniform(CABIN_TEMP_BOTTOM_LIMIT, CABIN_TEMP_TOP_LIMIT), TEMPERATURE_ROUNDING)
         message = createMessage(CABIN_TEMP_ID, cabin_temperature)
-        datafile.write(message)
+        channel.basic_publish(exchange='', routing_key=QUEUE_NAME, body=message)
 
         outside_temperature = round(random.uniform(OUTSIDE_TEMP_BOTTOM_LIMIT, OUTSIDE_TEMP_TOP_LIMIT), TEMPERATURE_ROUNDING)
         message = createMessage(OUTSIDE_TEMP_ID, outside_temperature)
-        datafile.write(message)
+        channel.basic_publish(exchange='', routing_key=QUEUE_NAME, body=message)
 
         intake_air_temperature = round(random.uniform(INTAKE_AIR_TEMP_BOTTOM_LIMIT, INTAKE_AIR_TEMP_TOP_LIMIT), TEMPERATURE_ROUNDING)
         message = createMessage(INTAKE_AIR_ID, intake_air_temperature)
-        datafile.write(message)
+        channel.basic_publish(exchange='', routing_key=QUEUE_NAME, body=message)
 
         # barometer data
         rl_pressure = random.randint(PRESSURE_BOTTOM_LIMIT, PRESSURE_TOP_LIMIT)
         message = createMessage(REAR_LEFT_TIRE_ID, rl_pressure)
-        datafile.write(message)
+        channel.basic_publish(exchange='', routing_key=QUEUE_NAME, body=message)
 
         rr_pressure = random.randint(PRESSURE_BOTTOM_LIMIT, PRESSURE_TOP_LIMIT)
         message = createMessage(REAR_RIGHT_TIRE_ID, rr_pressure)
-        datafile.write(message)
+        channel.basic_publish(exchange='', routing_key=QUEUE_NAME, body=message)
 
         fl_pressure = random.randint(PRESSURE_BOTTOM_LIMIT, PRESSURE_TOP_LIMIT)
         message = createMessage(FRONT_LEFT_TIRE_ID, fl_pressure)
-        datafile.write(message)
+        channel.basic_publish(exchange='', routing_key=QUEUE_NAME, body=message)
 
         fr_pressure = random.randint(PRESSURE_BOTTOM_LIMIT, PRESSURE_TOP_LIMIT)
         message = createMessage(FRONT_RIGHT_TIRE_ID, fr_pressure)
-        datafile.write(message)
+        channel.basic_publish(exchange='', routing_key=QUEUE_NAME, body=message)
 
         # potentiometer data
         gas_pedal = random.randint(PERCENT_BOTTOM_LIMIT, PERCENT_TOP_LIMIT)
         message = createMessage(GAS_PEDAL_POSITION_ID, gas_pedal)
-        datafile.write(message)
+        channel.basic_publish(exchange='', routing_key=QUEUE_NAME, body=message)
 
         wheel_angle = random.randint(PERCENT_BOTTOM_LIMIT, PERCENT_TOP_LIMIT)
         message = createMessage(STERRING_WHEEL_ANGLE_ID, wheel_angle)
-        datafile.write(message)
+        channel.basic_publish(exchange='', routing_key=QUEUE_NAME, body=message)
 
         electric_battery = random.randint(PERCENT_BOTTOM_LIMIT, PERCENT_TOP_LIMIT)
         message = createMessage(ELECTRIC_VEHICLE_BATTERY_ID, electric_battery)
-        datafile.write(message)
+        channel.basic_publish(exchange='', routing_key=QUEUE_NAME, body=message)
 
         radio_volume = random.randint(PERCENT_BOTTOM_LIMIT, PERCENT_TOP_LIMIT)
         message = createMessage(RADIO_VOLUME_ID, radio_volume)    
-        datafile.write(message)
+        channel.basic_publish(exchange='', routing_key=QUEUE_NAME, body=message)
 
         # parking distance data
         rl_dist = random.randint(PARKING_BOTTOM_LIMIT, PARKING_TOP_LIMIT)
         message = createMessage(REAR_LEFT_DIST_ID, rl_dist) 
-        datafile.write(message)
+        channel.basic_publish(exchange='', routing_key=QUEUE_NAME, body=message)
 
         rr_dist = random.randint(PARKING_BOTTOM_LIMIT, PARKING_TOP_LIMIT)
         message = createMessage(REAR_RIGHT_DIST_ID, rr_dist) 
-        datafile.write(message)
+        channel.basic_publish(exchange='', routing_key=QUEUE_NAME, body=message)
 
         rc_dist = random.randint(PARKING_BOTTOM_LIMIT, PARKING_TOP_LIMIT)
         message = createMessage(REAR_CENTER_DIST_ID, rc_dist) 
-        datafile.write(message)
+        channel.basic_publish(exchange='', routing_key=QUEUE_NAME, body=message)
 
         fc_dist = random.randint(PARKING_BOTTOM_LIMIT, PARKING_TOP_LIMIT)
         message = createMessage(FRONT_CENTER_DIST_ID, fc_dist) 
-        datafile.write(message)
+        channel.basic_publish(exchange='', routing_key=QUEUE_NAME, body=message)
         
         print("Sensor values sent")
         time.sleep(5)
+
+except KeyboardInterrupt:
+    print("Sensor values sending was interrupted")
+
+finally:
+    connection.close()
